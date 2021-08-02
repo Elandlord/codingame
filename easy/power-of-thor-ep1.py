644366051346644c66296b78a1104293fa7d2b75
Eric Landheer <e.landheer@mentechmedia.nl>
2021-08-02 10:43:04 +0200

    :rainbow: Add power of thor ep1 solution

---
+++ easy/power-of-thor-ep1.py
@@ -0,0 +1,34 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+# ---
+# Hint: You can use the debug stream to print initialTX and initialTY, if Thor seems not follow your orders.
+
+# light_x: the X position of the light of power
+# light_y: the Y position of the light of power
+# initial_tx: Thor's starting X position
+# initial_ty: Thor's starting Y position
+light_x, light_y, initial_tx, initial_ty = [int(i) for i in input().split()]
+
+while True:
+    remaining_turns = int(input())
+
+    direction = ""
+
+    if initial_ty < light_y:
+        direction += "S"
+        initial_ty += 1
+    elif initial_ty > light_y:
+        direction += "N"
+        initial_ty -= 1
+
+    if initial_tx < light_x:
+        direction += "E"
+        initial_tx += 1
+    elif initial_tx > light_x:
+        direction += "W"
+        initial_tx -= 1
+
+    print(direction)
