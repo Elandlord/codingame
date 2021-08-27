0168a8541ce1e6b33238556952a772c0222c672e
Eric Landheer <e.landheer@mentechmedia.nl>
2021-08-27 12:54:17 +0200

    :rainbow: Binary extensions tree with coords

--- challenge/binary-extension-tree-with-coords.js
+++ challenge/binary-extension-tree-with-coords.js
@@ -20,93 +20,163 @@ let goals = [...Array(goalsCount)].map(() => {
     };
 });
 
-class Node {
-    constructor(val) {
-        this.val = val;
+class Node{
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
         this.x = null;
         this.y = null;
-        this.right = null;
-        this.left = null;
-        this.count = 0;
     };
 };
 
-class BinarySearchTree {
-    constructor() {
+class BinarySearchTree{
+
+    constructor(){
         this.root = null;
     }
 
-    create(val) {
-        const newNode = new Node(val);
-
-        if (!this.root) {
+    insert(data){
+        var newNode = new Node(data);
+        if(this.root === null){
             this.root = newNode;
-            return this;
-        };
-
-        let current = this.root;
-
-        const addSide = side => {
-            if (!current[side]) {
-                current[side] = newNode;
-                return this;
-            };
-
-            current = current[side];
-        };
-
-        while (true) {
-            if (val === current.val) {
-                current.count++;
-                return this;
-            };
-
-            val < current.val ? addSide('left') : addSide('right');
+        }else{
+            this.insertNode(this.root, newNode);
         };
     };
 
-    find(val) {
-        if (!this.root) return undefined;
-
-        let current = this.root, found = false;
-
-        while (current && !found) {
-            if (val < current.val) current = current.left;
-            else if (val > current.val) current = current.right;
-            else found = true;
+    insertNode(node, newNode){
+        if(newNode.data < node.data){
+            if (node.left === null){
+                node.left = newNode;
+            } else{
+                this.insertNode(node.left, newNode);
+            };
+        } else {
+            if (node.right === null){
+                node.right = newNode;
+            } else{
+                this.insertNode(node.right,newNode);
+            };
         };
+    };
 
-        if (!found) return 'Nothing Found!';
-        return current;
-    }
-
-    findByCoords(x, y) {
-        // find the node by coords
-        // find the left/right node to traverse to
-        // if root? End the loop and reverse the array.
-    }
-
-
-    nodeCoordinates(node, x, y) {
-        if (x === undefined && y === undefined ) {x = 0; y = 0;}
-        if (!node) {return;}
-
-        node.x = x;
-        node.y = y;
-
-        this.nodeCoordinates(node.left, --x, --y);
-        this.nodeCoordinates(node.right, x+=2, y--);
-    }
 };
 
-let tree = new BinarySearchTree();
+function hasPath(root, arr, x)
+{
+    if (root == null)
+        return false;
 
-for(let i = n; i > 0; i--) {
-    tree.create(i);
+    arr.push(root.data);
+
+    if (root.data == x)
+        return true;
+
+    if ((hasPath(root.left, arr, x) ||
+        hasPath(root.right, arr, x)) && bombs.filter(bomb => root.x === bomb.x && root.y).length === 0)
+        return true;
+
+    arr.pop();
+    return false;
 }
 
-tree.nodeCoordinates(tree.find(n));
+function getPath(root, x)
+{
+    let arr = [];
+
+    if (hasPath(root, arr, x)) {
+        for(let i = 0; i < arr.length - 1; i++) {
+            console.log(arr[i]);
+        }
+        console.log(arr[arr.length - 1]);
+    }
+}
+
+
+function setCoordinates(node) {
+    if(node.left != null) {
+        node.left.x = node.x-1;
+        node.left.y = node.y+1;
+        setCoordinates(node.left);
+    }
+
+    if(node.right != null) {
+        node.right.x = node.x+1;
+        node.right.y = node.y+1;
+        setCoordinates(node.right);
+    }
+}
+
+function permute(permutation) {
+    var length = permutation.length,
+        result = [permutation.slice()],
+        c = new Array(length).fill(0),
+        i = 1, k, p;
+
+    while (i < length) {
+        if (c[i] < i) {
+            k = i % 2 && c[i];
+            p = permutation[i];
+            permutation[i] = permutation[k];
+            permutation[k] = p;
+            ++c[i];
+            i = 1;
+            result.push(permutation.slice());
+        } else {
+            c[i] = 0;
+            ++i;
+        }
+    }
+    return result;
+}
+
+let numbers = [];
+
+for(i = 1; i <= n; i++) {
+    numbers.push(i);
+}
+
+let permutations = permute(numbers);
 
 goals.forEach((goal) => {
-    tree.findByCoords(goal.x, goal.y);
+    console.error(goal);
+    permutations.forEach((permutation) => {
+        let tree = new BinarySearchTree();
+
+        permutation.forEach(number => tree.insert(number));
+
+        tree.root.x = (width-1)/2
+        tree.root.y = 0;
+
+        setCoordinates(tree.root);
+
+        let nodes = [tree.root];
+
+        function getNodes(root) {
+            if (root == null) return;
+
+            if (root.left == null && root.right == null) return;
+
+            if (root.left != null) {
+                nodes.push(root.left);
+                getNodes(root.left);
+            }
+
+            if (root.right != null) {
+                nodes.push(root.right);
+                getNodes(root.right);
+            }
+        }
+
+        getNodes(tree.root);
+
+        let value = nodes.filter(node => node.x === goal.x && node.y === goal.y)[0];
+
+        if(value === undefined) return;
+
+        getPath(tree.root, value.data);
+    });
 });
+
+
