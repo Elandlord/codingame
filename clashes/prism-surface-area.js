694db44e87af6359128c9c04cc084a332fe9714c
Eric Landheer <e.landheer@mentechmedia.nl>
2021-09-04 09:37:48 +0200

    :rainbow: Find surface area of prism

---
+++ clashes/prism-surface-area.js
@@ -0,0 +1,5 @@
+const length = parseInt(readline());
+const width = parseInt(readline());
+const height = parseInt(readline());
+
+console.log(2 * (width*height) + 2 * (length*height) + 2 * (length*width));
